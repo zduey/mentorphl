cf914e979e6f592b84ea0fae779a2429ebe198c3
Zach Duey <zachduey@gmail.com>
2017-11-12 19:59:28 -0500

    add phone number to user account

--- mentorphl/accounts/forms.py
+++ mentorphl/accounts/forms.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254)
+    phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$')
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'phone_number', 'password1', 'password2')
