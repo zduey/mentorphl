1baf5e1b9c7fc922c28c24f36b6b94cff05f2342
Zach Duey <zachduey@gmail.com>
2017-11-26 22:46:47 -0500

    Alternative to db config for production

--- mentorphl/config/settings/production.py
+++ mentorphl/config/settings/production.py
@@ -1,17 +1,11 @@
+import dj_database_url
 from .base import *
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mentorphilly',
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '',
-        'PORT': '',
-    }
-}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
