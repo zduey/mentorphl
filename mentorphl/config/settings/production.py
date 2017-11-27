d1fa1d52f42d2c402de24186575f86e86f8c53bf
Zach Duey <zachduey@gmail.com>
2017-11-26 23:02:33 -0500

    trying something else

--- mentorphl/config/settings/production.py
+++ mentorphl/config/settings/production.py
@@ -2,20 +2,7 @@ from .base import *
 
 DEBUG = False
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mentorphilly',
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
